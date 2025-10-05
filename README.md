e47b46e39c3303d100083022d07b8edf221e0407
Lennardo <168543710+DasStilleKind@users.noreply.github.com>
2025-10-05 20:26:04 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Browser-Start-Page
+
+
+# Awesome Homepage 🌟
+
+Welcome to our awesome homepage project! 
+Its inspired by google's material design 3,
+This is more than just a homepage – it's your personalized gateway to the web.
+
+
+The homepage features a **clean and modern** *material design* with a personalized greeting, a dynamic search bar, and sleek, customizable widgets for weather and time, ensuring a user-friendly and visually appealing experience.
+
+## You can try it here:
+https://my-webpage01.netlify.app
+
+https://ronak-08.github.io/Web-Homepage
+
+## Features 🚀
+
+- **Mobile Friendly:** Our homepage is optimized for mobile devices, ensuring a seamless and responsive user experience across all screen sizes.
+- **Interactive Icons:** Quickly access your favorite websites with just a click.
+- **Dynamic Clock:** Always stay on schedule with a sleek, real-time clock.
+- **Customizable:** Personalize your experience by changing your name and accent color right on the homepage.
+- **Responsive Design:** Seamlessly adapts to any screen size for a consistent experience on desktop and mobile.
+- **Weather:** Provides Latest Weather of your Location.
+- **Latest News:** Stay informed with the latest headlines conveniently displayed.
+- **Random Background Image:** Random image in background, toogle it in settings.
+- **Custom Accent Color:** make your favourite color the accent of the entire website
+- **Custom Logo** Add your favourite image to the logo.
+
+**Note:** ***Click on name to input new name***
+  
+## Screenshots 📷
+
+![screenshot](Images/Screenshot_20240710-171228.png)
+
+![screenshot](Images/Screenshot_20240629-140049.png)
+
+![screenshot](Images/Screenshot_20240629-140557.png)
+
+## Get Started 🛠️
+
+1. Clone the repository.
+2. Open `index.html` in your web browser.
+3. Enjoy exploring the features!
+
+## Contributing 🤝
+
+We welcome contributions! If you have ideas for improvements or new features, feel free to fork the repository and submit a pull request.
+
+## Feedback 💬
+
+Have feedback or questions? I'd love to hear from you! Reach out to me via ronakameta65@gmail.com
+
---
+++ scripts.js
@@ -0,0 +1,381 @@
+// --- Uhrzeit aktualisieren ---
+function updateTime() {
+  const timeElement = document.getElementById('timeElement');
+  const currentDate = new Date();
+
+  let hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+
+  const formattedTime = `${hours}:${formattedMinutes}`;
+  timeElement.textContent = formattedTime;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+
+// --- Datum anzeigen ---
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const today = new Date();
+const dayOfWeek = days[today.getDay()];
+const dateOfMonth = today.getDate();
+const formattedDate = `${dayOfWeek} ${dateOfMonth}`;
+document.getElementById('currentDay').textContent = formattedDate;
+
+// --- Wetter API mit festem Cityname 'Delligsen' ---
+const apiKey = 'db5a5dd4233284f22f468c35dad5f8ff';
+
+async function getWeather() {
+  const fixedCity = 'Delligsen';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${fixedCity}&appid=${apiKey}&units=metric`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (data.cod === 200) {
+    const cityName = data.name;
+    const temperature = Math.round(data.main.temp);
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+
+    updateWeatherInfo(cityName, temperature, description, icon);
+  } else {
+    console.log('City not found. Please check the city name and try again.');
+  }
+}
+
+function updateWeatherInfo(cityName, temperature, description, icon) {
+  document.getElementById('city-name').textContent = cityName;
+  document.getElementById('temperature').textContent = `${temperature}°`;
+  document.getElementById('description').textContent = description;
+
+  // Icon aus festem Verzeichnis laden (z.B. /icons/01d.png)
+  const iconUrl = `/icons/${icon}.png`;
+  document.getElementById('weather-icon').src = iconUrl;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  getWeather();
+});
+
+// --- Name fest auf "Lennart" ---
+window.onload = function() {
+  document.getElementById('name').textContent = 'Lennart';
+};
+
+// --- Hintergrundbild Checkbox ---
+const checkbox = document.getElementById('backgroundToggle');
+
+function setBackground() {
+    const imageUrl = 'https://picsum.photos/1920/1080/?random&blur';
+    document.documentElement.style.backgroundImage = `url(${imageUrl})`;
+    document.documentElement.style.backgroundSize = 'cover';
+}
+
+function handleCheckboxChange() {
+    if (checkbox.checked) {
+        localStorage.setItem('backgroundToggle', 'checked');
+        setBackground();
+    } else {
+        localStorage.removeItem('backgroundToggle');
+        document.body.style.backgroundImage = '';
+        document.body.style.backgroundColor = '#121212';
+    }
+}
+
+if (localStorage.getItem('backgroundToggle') === 'checked') {
+    checkbox.checked = true;
+    setBackground();
+}
+
+checkbox.addEventListener('change', handleCheckboxChange);
+
+setInterval(() => {
+    if (checkbox.checked) {
+        setBackground();
+    }
+}, 3600000);
+
+// --- Farbe setzen und speichern ---
+function setColorAndStore(color) {
+    document.documentElement.style.setProperty('--accent', color);
+    localStorage.setItem('selectedColor', color);
+}
+
+document.getElementById('colorPicker').addEventListener('input', function() {
+    let selectedColor = this.value;
+    setColorAndStore(selectedColor);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    let storedColor = localStorage.getItem('selectedColor');
+    if (storedColor) {
+        setColorAndStore(storedColor);
+    }
+});
+
+// --- Farbe mit RGBA für srcbar ---
+function setColorAndStor(color) {
+    document.documentElement.style.setProperty('--srcbar', color);
+    localStorage.setItem('selectedColo', color);
+}
+
+document.getElementById('colorPicker').addEventListener('input', function() {
+    let hexColor = this.value;
+    let rgbaColor = hexToRgba(hexColor, 0.429);
+    setColorAndStor(rgbaColor);
+});
+
+function hexToRgba(hex, alpha) {
+    hex = hex.replace(/^#/, '');
+    let bigint = parseInt(hex, 16);
+    let r = (bigint >> 16) & 255;
+    let g = (bigint >> 8) & 255;
+    let b = bigint & 255;
+    return `rgba(${r},${g},${b},${alpha})`;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let storedColo = localStorage.getItem('selectedColo');
+    if (storedColo) {
+        setColorAndStor(storedColo);
+    }
+});
+
+// --- News Toggle ---
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.getElementById('toggle');
+    var container = document.querySelector('.container');
+
+    var isChecked = localStorage.getItem('newsDisabled') === 'true';
+    checkbox.checked = isChecked;
+    updateContainerVisibility(isChecked);
+
+    function updateContainerVisibility(disabled) {
+        container.style.display = disabled ? "none" : "flex";
+    }
+
+    checkbox.addEventListener('change', function() {
+        var isChecked = checkbox.checked;
+        localStorage.setItem('newsDisabled', isChecked);
+        updateContainerVisibility(isChecked);
+    });
+});
+
+// --- Sticky Search Container ---
+window.addEventListener('scroll', function() {
+    var searchContainer = document.getElementById('searchContainer');
+    if (window.pageYOffset > 310) {
+        searchContainer.classList.add('sticky');
+    } else {
+        searchContainer.classList.remove('sticky');
+    }
+});
+
+// --- Links Verwaltung ---
+let deleteMode = false;
+
+function toggleDeleteMode() {
+    deleteMode = !deleteMode;
+    const linkContainers = document.querySelectorAll('.link-container');
+    linkContainers.forEach(container => {
+        const deleteButton = container.querySelector('.delete-button');
+        deleteButton.style.display = deleteMode ? 'block' : 'none';
+        container.classList.toggle('delete-mode', deleteMode);
+    });
+}
+
+function addLink() {
+    const url = document.getElementById('urlInput').value;
+    if (!url) return;
+
+    const faviconUrl = `https://www.google.com/s2/favicons?domain=${url}`;
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.target = '_blank';
+    link.innerHTML = `<img src="${faviconUrl}" alt="icon">`;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'X';
+    deleteButton.className = 'delete-button';
+    deleteButton.style.display = deleteMode ? 'block' : 'none';
+    deleteButton.onclick = function() {
+        deleteLink(url);
+    };
+
+    const linkContainer = document.createElement('div');
+    linkContainer.className = 'link-container';
+    linkContainer.appendChild(link);
+    linkContainer.appendChild(deleteButton);
+
+    document.getElementById('iconsContainer').appendChild(linkContainer);
+
+    const links = JSON.parse(localStorage.getItem('links')) || [];
+    links.push({ url: url });
+    localStorage.setItem('links', JSON.stringify(links));
+
+    document.getElementById('urlInput').value = '';
+}
+
+function loadLinks() {
+    const links = JSON.parse(localStorage.getItem('links')) || [];
+    const iconsContainer = document.getElementById('iconsContainer');
+
+    links.forEach(linkData => {
+        const faviconUrl = `https://www.google.com/s2/favicons?domain=${linkData.url}`;
+
+        const link = document.createElement('a');
+        link.href = linkData.url;
+        link.target = '_blank';
+        link.innerHTML = `<img src="${faviconUrl}" alt="icon">`;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = 'X';
+        deleteButton.className = 'delete-button';
+        deleteButton.style.display = deleteMode ? 'block' : 'none';
+        deleteButton.onclick = function() {
+            deleteLink(linkData.url);
+        };
+
+        const linkContainer = document.createElement('div');
+        linkContainer.className = 'link-container';
+        linkContainer.appendChild(link);
+        linkContainer.appendChild(deleteButton);
+
+        iconsContainer.appendChild(linkContainer);
+    });
+}
+
+function deleteLink(url) {
+    const iconsContainer = document.getElementById('iconsContainer');
+    const linkContainers = iconsContainer.getElementsByClassName('link-container');
+
+    for (let i = 0; i < linkContainers.length; i++) {
+        const link = linkContainers[i].getElementsByTagName('a')[0];
+        if (link && link.href === url) {
+            iconsContainer.removeChild(linkContainers[i]);
+            break;
+        }
+    }
+
+    let links = JSON.parse(localStorage.getItem('links')) || [];
+    links = links.filter(linkData => linkData.url !== url);
+    localStorage.setItem('links', JSON.stringify(links));
+}
+
+window.onload = loadLinks;
+
+// --- Suchmaschinen Auswahl ---
+const searchEngineSelector = document.getElementById('searchEngine');
+const searchForm = document.getElementById('searchForm');
+const searchInput = document.getElementById('searchInput');
+
+const savedEngine = localStorage.getItem('selectedSearchEngine');
+if (savedEngine) {
+  searchEngineSelector.value = savedEngine;
+  searchForm.action = savedEngine;
+  const selectedOptionText = searchEngineSelector.options[searchEngineSelector.selectedIndex].text.trim();
+  searchInput.placeholder = `Search on ${selectedOptionText}...`;
+}
+
+searchEngineSelector.addEventListener('change', function() {
+  const selectedEngine = this.value;
+  searchForm.action = selectedEngine;
+  const selectedOptionText = this.options[this.selectedIndex].text.trim();
+  searchInput.placeholder = `Search on ${selectedOptionText}...`;
+  localStorage.setItem('selectedSearchEngine', selectedEngine);
+});
+
+searchInput.addEventListener('keypress', function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    searchForm.submit();
+  }
+});
+
+document.getElementById('searchInput').addEventListener('input', function() {
+    let query = this.value;
+
+    if (query.length > 0) {
+        fetchSuggestions(query);
+    } else {
+        clearSuggestions();
+    }
+});
+
+function fetchSuggestions(query) {
+    const url = `https://suggestqueries.google.com/complete/search?client=chrome&q=${encodeURIComponent(query)}&callback=handleSuggestions`;
+
+    const script = document.createElement('script');
+    script.src = url;
+    document.body.appendChild(script);
+    document.body.removeChild(script);
+}
+
+function handleSuggestions(data) {
+    displaySuggestions(data[1]);
+}
+
+function displaySuggestions(suggestions) {
+    const suggestionsList = document.getElementById('suggestions');
+    clearSuggestions();
+
+    suggestions.forEach(suggestion => {
+        const suggestionItem = document.createElement('p');
+        suggestionItem.textContent = suggestion;
+        suggestionItem.classList.add('suggestion-item');
+        suggestionItem.addEventListener('click', function() {
+            document.getElementById('searchInput').value = suggestion;
+            clearSuggestions();
+            document.getElementById('searchForm').submit();
+        });
+        suggestionsList.appendChild(suggestionItem);
+    });
+}
+
+function clearSuggestions() {
+    const suggestionsList = document.getElementById('suggestions');
+    suggestionsList.innerHTML = '';
+}
+
+// --- Avatar Upload ---
+const avatar = document.querySelector('.avatar');
+const fileInput = document.getElementById('file-input');
+
+const storedImage = localStorage.getItem('uploadedAvatar');
+if (storedImage) {
+    avatar.src = storedImage;
+}
+
+avatar.addEventListener('click', () => {
+    fileInput.click();
+});
+
+fileInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const result = e.target.result;
+            avatar.src = result;
+            localStorage.setItem('uploadedAvatar', result);
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+// --- Tabs ---
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
